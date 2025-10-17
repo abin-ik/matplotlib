27b7d9f3ddf2dfae9ddd3ca0372e1988b737c273
Abin Thomas <abin29192@gmail.com>
2025-10-17 12:07:51 +0530

    Draw a histogram to display the frequency of 'Purchase Amount' in the dataset.

---
+++ oct17d.py
@@ -0,0 +1,18 @@
+# Sample Dataset ⿤: Purchase Amounts
+
+import matplotlib.pyplot as plt
+import pandas as pd
+
+
+df_purchase = pd.DataFrame({
+    'Purchase Amount': [120, 250, 300, 150, 500, 600, 800, 400, 350, 700, 900, 1000, 150, 200, 650]
+})
+
+# ⿤ Draw a histogram to display the frequency of 'Purchase Amount' in the dataset.
+#     - Use edgecolor='black' for better visibility of bins.
+#     - Add gridlines to make it more readable.
+
+
+plt.hist(df_purchase["Purchase Amount"],bins=10,edgecolor='green')
+plt.grid(color = "black")
+plt.show()
